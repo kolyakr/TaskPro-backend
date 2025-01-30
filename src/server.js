ee6c3d434cec1de1673085b5ee44f47fb4b4ae38
Mykola Kryvoruchko <krivoruchko.kolya@gmail.com>
2025-01-30 13:41:18 +0200

    add dev and prod

--- src/server.js
+++ src/server.js
@@ -1,15 +1,29 @@
 import express from 'express';
-import { env } from './utils/env.js';
+import dotenv from 'dotenv';
 import cors from 'cors';
 import pino from 'pino-http';
 import { notFound } from './middlewares/notFound.js';
 import { errorHandler } from './middlewares/errorHandler.js';
-import { ENV } from './constants.js';
 import { rootRouter } from './routes/index.js';
 import cookieParser from 'cookie-parser';
 
+dotenv.config();
+
 export const startServer = () => {
   const app = express();
+
+  const isProduction = process.env.NODE_ENV === 'production';
+
+  const allowedOrigins = isProduction
+    ? [process.env.FRONTEND_URL_PROD]
+    : [process.env.FRONTEND_URL_DEV];
+
+  app.use((req, res, next) => {
+    console.log('📌 Новий запит:', req.method, req.url);
+    console.log('🔹 Origin:', req.headers.origin || 'N/A');
+    console.log('🔹 Cookies:', req.cookies || 'N/A');
+    next();
+  });
 
   app.use(
     pino({
@@ -24,7 +38,13 @@ export const startServer = () => {
 
   app.use(
     cors({
-      origin: 'https://task-pro-frontend-pied.vercel.app',
+      origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
       credentials: true,
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
       allowedHeaders: ['Content-Type', 'Authorization'],
@@ -34,10 +54,13 @@ export const startServer = () => {
   app.use(rootRouter);
 
   app.use(notFound);
+
   app.use(errorHandler);
 
-  const PORT = env(ENV.PORT, 3000);
+  const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+    console.log(
+      `🚀 Сервер працює на порту ${PORT} у режимі ${process.env.NODE_ENV}`,
+    );
   });
 };
